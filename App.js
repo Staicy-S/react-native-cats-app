9bb42a730bffb8ec4a47e2a67f3f815a0094cbf0
Staicy Stadtler <staicy@hotmail.de>
2024-07-09 14:44:23 +0200

    content and layout done

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { Image } from "expo-image";
 
 export default function App() {
   return (
@@ -11,8 +12,36 @@ export default function App() {
           <Text style={styles.navigationMenues}>Gallery</Text>
           <Text style={styles.navigationMenues}>Contact</Text>
         </View>
-        <Text style={styles.heading}>Cats 🐱</Text>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={styles.main}>
+          <View style={styles.mainUpperPart}>
+            <Text style={styles.heading}>Cats 🐱</Text>
+            <Text style={styles.subHeading}>
+              Everything you need to know about Cats
+            </Text>
+            <Text style={styles.paragraph}>
+              Cats are very lovely animals. Treat them with kindness and respect
+              and they will reward you with affection! Sometimes, you can meet
+              cats in the wild.{"\n"}
+              {"\n"}I have been friends with a cat for a very long time, nearly
+              10 years, until she unfortunately passed away. I encountered him
+              every week before going to work. He was a very big cat! He loved
+              my backpack and took many opportunities to make himself
+              comfortable on it. {"\n"}
+              {"\n"}You never know if a cat you meet might become a dear friend
+              to you. So enjoy every cat encounter you can get 😻
+            </Text>
+          </View>
+          <Image
+            source="https://images.unsplash.com/photo-1511044568932-338cba0ad803?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            style={styles.image}
+          />
+          <Pressable
+            onPress={() => console.log("pressed")}
+            style={styles.button}
+          >
+            <Text style={styles.buttonText}>Go to Gallery</Text>
+          </Pressable>
+        </View>
         <View style={styles.footer}>
           <Text style={styles.footerMenues}>Datenschutz</Text>
           <Text style={styles.footerMenues}>Impressum</Text>
@@ -43,14 +72,45 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
   heading: {
-    fontSize: 32,
+    fontSize: 40,
+  },
+  subHeading: {
+    fontSize: 20,
+  },
+  main: {
+    padding: 10,
+    alignItems: "center",
+    gap: 20,
+  },
+  mainUpperPart: {
+    height: 300,
+    alignItems: "center",
+  },
+  paragraph: {
+    textAlign: "left",
+  },
+  image: {
+    width: 220,
+    height: 220,
+    borderWidth: 5,
+    borderRadius: 20,
+  },
+  button: {
+    backgroundColor: "orange",
+    padding: 15,
+    borderRadius: 20,
+  },
+  buttonText: {
+    fontSize: 20,
   },
   footer: {
     flexDirection: "row",
     justifyContent: "space-between",
     width: "100%",
     padding: 10,
-    backgroundColor: "beige",
+    backgroundColor: "darkorange",
   },
-  footerMenues: {},
+  footerMenues: {
+    color: "white",
+  },
 });
